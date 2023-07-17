6a11350b16b8fc25d4ba201e38477428f4f616d8
dylanczt06 <dylanchan1234@gmail.com>
2023-07-17 11:38:50 +0800

    initial commit

---
+++ Assets/GM.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+#region SINGLETON PATTERN
+    public static GM _instance;
+    public static GM I
+    {
+        get {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GM>();
+                
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject("Game Manager");
+                    _instance = container.AddComponent<GM>();
+                }
+            }
+        
+            return _instance;
+        }
+    }
+#endregion
+
+    public PlayerManager player;
+    public EnnemyManager ennemy;
+    public CameraManager cam;
+    public UIManager ui;
+    public AudioManager audioManager;
+
+    public Material blinkMat;
+    public Material spriteMat;
+
+    public List<GameObject> bosses;
+
+
+    public static PlayerManager Player => GM.I.player;
+    public static EnnemyManager Ennemies => GM.I.ennemy;
+    public static CameraManager Cam => GM.I.cam;
+    public static UIManager UI => GM.I.ui;
+    public static AudioManager Audio => GM.I.audioManager;
+
+}
---
+++ Assets/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public float size;
+    public SpriteRenderer filler;
+    public float speed;
+    
+    float TargetValue = 0;
+
+    public void SetValue(float value, float max){
+        TargetValue = value * (size/max); 
+    }
+
+    void Update()
+    {
+        filler.size = new Vector2(Mathf.Lerp(filler.size.x, TargetValue, Time.deltaTime * speed), filler.size.y);
+    }
+}
